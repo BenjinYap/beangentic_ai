0d4177080d3a09b498b0e533c1eec78901085bfb
Benjin Yap <benjyap7+github@gmail.com>
2025-06-01 21:51:09 -0400

    basic window test

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-// const { app, BrowserWindow } = require('electron');
-import { app, BrowserWindow} from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import * as path from "node:path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let win;
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('dist/index.html')
@@ -13,3 +23,15 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
 })
+
+ipcMain.on('window:maximize', () => {
+  win.maximize();
+});
+
+ipcMain.on('window:minimize', () => {
+  win.minimize();
+});
+
+ipcMain.on('window:close', () => {
+  win.close();
+});
--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
 </script>
 
 <h1>hi</h1>
-<button class="border">
+<button class="border" onclick={() => window.electron.minimizeWindow()}>
   minimize
 </button>
-<button class="border">
+<button class="border" onclick={() => window.electron.maximizeWindow()}>
   maximize
 </button>
-<button class="border">
+<button class="border" onclick={() => window.electron.closeWindow()}>
   close
 </button>
+
+<p>{window.electron.chrome()} version</p>
